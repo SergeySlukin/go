db56b22ba77274f5bdcfa19dedc0d7757031b6bc
SergeySlukin <Borolginst@mail.ru>
2018-05-08 23:50:35 +0300

    upload

--- algorithms/insertion sorting/main.go
+++ algorithms/insertion sorting/main.go
@@ -5,14 +5,13 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{5, 2, 4, 6, 1, 3, 4,5,62,52,4,1,621,54}
-	for j := 1; j < len(a); j++ {
-		key := a[j]
-		i := j - 1
-		for ; i >= 0 && a[i] > key; i = i - 1 {
-			a[i + 1] = a[i]
+	a := []int{5, 2, 4, 6, 1, 3, 4, 5, 62, 52, 4, 1, 621, 54}
+	for k, v := range a {
+		i := k - 1
+		for ; i >= 0 && a[i] > v; i-- {
+			a[i+1] = a[i]
 		}
-		a[i + 1] = key
+		a[i+1] = v
 	}
 	fmt.Println(a)
 }
