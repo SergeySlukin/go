f51bcb80b56e43f3b8675889f6284048a79db904
Sergey Slukin <borolginst@mail.ru>
2018-09-02 21:13:25 +0300

    Changes parseString

--- AccessLogReader/src/parseString.go
+++ AccessLogReader/src/parseString.go
@@ -11,8 +11,6 @@ var uniqueUrls = make(map[string]uint64)
 
 var urlRegexp = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
 var httpInfo = regexp.MustCompile(`HTTP\/[0-9](\.[0-9])?\"\s[0-9]*\s[0-9]*`)
-//var test = regexp.MustCompile(`HTTP\/[0-9](\.[0-9])?\"\s[0-9]*(\s\-\s[0-9]*)?\s[0-9]*(\s\"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))?`)
-//var httpInfo = regexp.MustCompile(`HTTP\/[0-9](\.[0-9])?\"\s[0-9]*\s[0-9]*`)
 var searchEngineRegexp = regexp.MustCompile(`\)\s[a-zA-Z0-9]*`)
 
 /**
@@ -23,44 +21,14 @@ func stringHandler(s string) {
 	if !ok {
 		panic(errors.New("error parse url"))
 	}
-	//statusCode, b, url, _ := reT(s)
 	statusCode, bytes, _ := parseHttpInfo(s)
 
 	searchEngine, ok := parseSearchEngine(s)
 	if !ok {
 		searchEngine = "direct"
 	}
-	//addStatistics(url, statusCode, b, searchEngine)
 	addStatistics(res, statusCode, bytes, searchEngine)
 }
-
-/**
-@return
-statusCode uint16
-
- */
-/*func reT(s string) (uint16, uint64, string, error)  {
-	res := test.FindString(s)
-	respArray := strings.Split(res, " ")
-	statusCode, err := strconv.Atoi(respArray[1])
-	if err != nil {
-		panic(err)
-	}
-
-	bytes := 0
-	index := 2
-	if statusCode == 301 || statusCode == 302 {
-		index = 3
-	}
-	bytes, err = strconv.Atoi(respArray[index])
-	if err != nil {
-		panic(err)
-	}
-
-	respUrl := respArray[index+1]
-
-	return uint16(statusCode), uint64(bytes), respUrl[1:], nil
-}*/
 
 /**
 Парсим урл
